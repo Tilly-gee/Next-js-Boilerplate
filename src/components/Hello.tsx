3b0c2705c1d004869856c3f261632ea516c690e0
Tilly-gee <104358587+Tilly-gee@users.noreply.github.com>
2024-07-24 14:37:19 +0100

    Update Hello.tsx

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -2,8 +2,6 @@ import { currentUser } from '@clerk/nextjs/server';
 import { getTranslations } from 'next-intl/server';
 
 const Hello = async () => {
-  const t = await getTranslations('Dashboard');
-  const user = await currentUser();
 
   return (
     <>
